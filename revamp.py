ce2f0fff98c906a67fa7bcd19cdfc3c34c727dd3
Funky Apple <itspanda360lol@gmail.com>
2026-05-06 15:55:22 +0800

    Implement track ID to name mapping and retrieval function
    Added a mapping of track IDs to names and a function to retrieve track names based on ID.

--- revamp.py
+++ revamp.py
@@ -43,9 +43,44 @@ PARTICIPANTS_PACKET_ID = 4
 PLAYER_NAME = os.getenv("PLAYER_NAME", getpass.getuser())
 PLAYER_NAME_DETECTED = False
 
+TRACK_ID_TO_NAME = {
+    0: "Melbourne",
+    2: "Shanghai",
+    3: "Sakhir (Bahrain)",
+    4: "Catalunya",
+    5: "Monaco",
+    6: "Montreal",
+    7: "Silverstone",
+    9: "Hungaroring",
+    10: "Spa",
+    11: "Monza",
+    12: "Singapore",
+    13: "Suzuka",
+    14: "Abu Dhabi",
+    15: "Texas",
+    16: "Brazil",
+    17: "Austria",
+    19: "Mexico",
+    20: "Baku (Azerbaijan)",
+    26: "Zandvoort",
+    27: "Imola",
+    29: "Jeddah",
+    30: "Miami",
+    31: "Las Vegas",
+    32: "Losail",
+    39: "Silverstone (Reverse)",
+    40: "Austria (Reverse)",
+    41: "Zandvoort (Reverse)",
+}
+
 TRACK_NAME = None
 TRACK_ID = None
 SESSION_TYPE = None
+
+def get_track_name(track_id):
+    if track_id is None:
+        return None
+    return TRACK_ID_TO_NAME.get(int(track_id))
 
 def extract_track_info_from_session_packet(pkt):
     track_id = parse_int(get_attr(pkt, "track_id", "m_trackId", default=None), None)
@@ -421,7 +456,7 @@ def handle_session_packet(pid, data, pkt_cls, race_active, race_started_at):
     if track_id is not None:
         TRACK_ID = track_id
     if track_name:
-        TRACK_NAME = track_name
+        TRACK_NAME = get_track_name(track_id)
 
     is_race_session = session_type in RACE_SESSION_TYPES
 
